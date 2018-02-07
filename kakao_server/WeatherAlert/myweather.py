57757ef36e30db679abe357733a77c3595057f03
canceryoon <photo9609@gmail.com>
2018-02-07 05:12:12 +0000

    날씨 커멘드 기능 구현 잘못된 주소입력, API 에러 예외 처리

--- kakao_server/WeatherAlert/myweather.py
+++ kakao_server/WeatherAlert/myweather.py
@@ -5,18 +5,36 @@ import json
 def getaddr():
     return {
                 'message': {
-                    'text': '\"서울시 영등포구\" 형식으로 입력해주세요.'
+                    'text': '\"서울 영등포구 여의도동\" 형식으로 입력해주세요.'
                 }
           }
 
 def getweather(targetaddr):
-    wurl = 'https://api2.sktelecom.com/weather/current/hourly'
-    wappKey = '4fb6735c-5fcc-4c33-82df-cc09d54d7088'
-    wheaders = {'Content-Type': 'application/json; charset=utf8', 'appKey' : wappKey} 
-    wparams = { "version": "1", "city": "서울", "county": "영등포구", "village": "여의도동"}
-    wjson = requests.get(wurl, params=wparams, headers=wheaders)
-    print(wjson.json())
+    addr = targetaddr.split()
+
+    if len(addr) is not 3: 
+        answer = "주소를 잘못 입력했습니다.\n\"서울 영등포구 여의도동\"\n 처럼 시,도 를 빼고 입력해주세요"
+    else:
+        wurl = 'https://api2.sktelecom.com/weather/current/hourly'
+        wappKey = '4fb6735c-5fcc-4c33-82df-cc09d54d7088'
+        wheaders = {'Content-Type': 'application/json; charset=utf8', 'appKey' : wappKey} 
+        wparams = { "version": "1", "city": addr[0], "county": addr[1], "village": addr[2]}
+        wjson = requests.get(wurl, params=wparams, headers=wheaders).json()
+
+        if not wjson["weather"]["hourly"] :
+            answer = "주소를 잘못 입력했습니다.\n\"서울 영등포구 여의도동\"\n 처럼 시,도 를 빼고 입력해주세요"
+        else:
+            answer = "현재 기온 : " + str(wjson["weather"]["hourly"][0]["temperature"]["tc"]) + " ℃\n" \
+                + "풍속 : " + str(wjson["weather"]["hourly"][0]["wind"]["wspd"]) + " m/s\n" \
+                + "구름 : " + str(wjson["weather"]["hourly"][0]["sky"]["name"])
+    print(answer)
+    print(targetaddr)
 
     return { 'message': {
-                'text': wjson.json() }
+                'text': answer },
+             'keyboard': {
+                'type': 'buttons',
+                'buttons' : ['날씨', '코인', '주식']
+            }
+
            }
